6ad446d4b8af36052205ad0a2d9a07c01f196de0
MD Pahlovi <108403206+mdpahlovi@users.noreply.github.com>
2023-04-07 22:19:51 +0600

    Booking Page Done

--- pages/book.js
+++ pages/book.js
@@ -2,13 +2,42 @@ import Header from "@/components/Common/Header";
 import Link from "next/link";
 import React from "react";
 
+const services = [
+    "Consultation On Digital Marketing",
+    "Seo And Website Management",
+    "Creative Graphic Solutions",
+    "Online Advertising Management",
+    "Social Media Management",
+    "Web Design & Development",
+];
+
 export default function book() {
     return (
         <>
-            <Header title="Our Recent Works">
-                <Link href="/book">Book</Link>
+            <Header title="Let's Partner up">
+                <Link href="/book">Booking</Link>
             </Header>
-            <section className="container py-16"></section>
+            <section className="container py-16">
+                <form className="max-w-2xl mx-auto flex flex-col gap-4">
+                    <input type="text" name="name" placeholder="Company Name" required />
+                    <input type="email" name="email" placeholder="Email" required />
+                    <select name="service">
+                        <option value="">Select a service</option>
+                        {services.map((service, idx) => (
+                            <option key={idx} value={service}>
+                                {service}
+                            </option>
+                        ))}
+                    </select>
+                    <div className="grid xs:grid-cols-2 gap-4">
+                        <input type="date" name="start" placeholder="Starting Date" />
+                        <input type="date" name="end" placeholder="Ending Date" />
+                    </div>
+                    <button type="submit" className="button primary justify-center">
+                        Submit Now
+                    </button>
+                </form>
+            </section>
         </>
     );
 }
--- pages/products.js
+++ pages/products.js
@@ -3,6 +3,8 @@ import Header from "@/components/Common/Header";
 import Link from "next/link";
 import Image from "next/image";
 import { SlSizeFullscreen } from "react-icons/sl";
+import { PhotoProvider, PhotoView } from "react-photo-view";
+import "react-photo-view/dist/react-photo-view.css";
 
 function classNames(...classes) {
     return classes.filter(Boolean).join(" ");
@@ -55,22 +57,26 @@ export default function Example() {
                     <Tab.Panels className="mt-8">
                         {categories.map(({ data }, idx) => (
                             <Tab.Panel key={idx} className="grid sm:grid-cols-2 lg:grid-cols-3 gap-8">
-                                {data.map((item, idx) => (
-                                    <div key={idx} className="relative group aspect-[4_/_3] card overflow-hidden flex justify-center items-center">
-                                        <SlSizeFullscreen
-                                            size={36}
-                                            className="absolute z-10 text-white opacity-0 group-hover:opacity-75 transition duration-300"
-                                        />
-                                        <Image
-                                            src={`/product/image (${item}).jpg`}
-                                            alt=""
-                                            className="absolute w-full h-full object-cover object-center group-hover:scale-125 transition duration-300"
-                                            width={512}
-                                            height={512}
-                                        />
-                                        <div className="absolute w-full h-full top-0 bg-black opacity-0 group-hover:opacity-75 transition duration-300"></div>
-                                    </div>
-                                ))}
+                                <PhotoProvider>
+                                    {data.map((item, idx) => (
+                                        <PhotoView key={idx} src={`/product/image (${item}).jpg`}>
+                                            <div className="relative group aspect-[4_/_3] card overflow-hidden flex justify-center items-center">
+                                                <SlSizeFullscreen
+                                                    size={36}
+                                                    className="absolute z-10 text-white opacity-0 group-hover:opacity-75 transition duration-300"
+                                                />
+                                                <Image
+                                                    src={`/product/image (${item}).jpg`}
+                                                    alt=""
+                                                    className="absolute w-full h-full object-cover object-center group-hover:scale-125 transition duration-300"
+                                                    width={512}
+                                                    height={512}
+                                                />
+                                                <div className="absolute w-full h-full top-0 bg-black opacity-0 group-hover:opacity-75 transition duration-300"></div>
+                                            </div>
+                                        </PhotoView>
+                                    ))}
+                                </PhotoProvider>
                             </Tab.Panel>
                         ))}
                     </Tab.Panels>
